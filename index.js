9eb28305a30f9843d13086a7d86adfcab7b63370
LCardoso <looricardoso@hotmail.com>
2020-10-09 15:55:04 -0300

    primeira versao

---
+++ index.js
@@ -0,0 +1,6 @@
+let nome = prompt('Digite seu nome: ');
+let sobrenome = prompt('Digite seu sobrenome: ');
+
+alert(`Nome completo: ${nome} ${sobrenome}`);
+sobrenome = sobrenome.toUpperCase();
+alert(`Nome de catálogo: ${sobrenome}, ${nome}`);
